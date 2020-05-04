7e62064142717d55dd0b196fe62b9ccb8adbf35f
Patrick Sodré <psodre@gmail.com>
2020-05-03 21:51:02 -0400

    Add fixture for testing db and cache_lru consumers

--- tests/c/test_approximate_memory_usage.py
+++ tests/c/test_approximate_memory_usage.py
@@ -4,14 +4,28 @@ from zeroae.rocksdb.c import approximate_memory_usage, memory_consumers
 
 
 @pytest.fixture
-def db_consumer(rocksdb_db, rocksdb_memory_consumers):
+def db_consumer(rocksdb_memory_consumers, rocksdb_db):
     memory_consumers.add_db(rocksdb_memory_consumers, rocksdb_db)
     yield rocksdb_memory_consumers
 
 
 @pytest.fixture
-def amu(db_consumer):
-    rv = approximate_memory_usage.create(db_consumer)
+def lru_consumer(rocksdb_memory_consumers, rocksdb_cache_lru):
+    memory_consumers.add_cache(rocksdb_memory_consumers, rocksdb_cache_lru)
+    yield rocksdb_memory_consumers
+
+
+@pytest.fixture(params=["db", "lru"])
+def consumers(request, db_consumer, lru_consumer):
+    if "db" == request.param:
+        yield db_consumer
+    if "lru" == request.param:
+        yield lru_consumer
+
+
+@pytest.fixture
+def amu(consumers):
+    rv = approximate_memory_usage.create(consumers)
     yield rv
     approximate_memory_usage.destroy(rv)
 
